d572f74c98695de36b34358dd5d9425d0c527c21
Zachary Weiss <zachary.weiss.acct@gmail.com>
2023-10-10 23:14:26 -0400

    Hacky backwards linking from dyn ptree to queriable metas

--- halfbakery.html.pm
+++ halfbakery.html.pm
@@ -1,4 +1,15 @@
 #lang pollen
+◊(require pollen/pagetree racket/list racket/format racket/string)
 
 Future home to half-baked ideas.
 
+◊(define (ptree-take-n folder file n [root "pagetree-root"] [ext ".pm"])
+   (map (λ(node) (string-append folder (symbol->string node) ext))
+        (take (children (string->symbol root) (get-pagetree (string-append folder file))) n)))
+
+◊(define (node->v node [field "date"])
+   (select-from-metas (string->symbol field) node))
+         
+◊(apply ul
+   (map (compose li node->v) (ptree-take-n "halfbakery/" "halfbakery.ptree" 3)))
+         
